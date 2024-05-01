cee62c6866ddbb9c49a567bcc8c341038799bd4f
Golferichs02 <jorge.rosalesd@udem.edu>
2024-05-01 11:22:55 -0600

    2nd commit Errores matematicos corregidos y modelo más robusto
    Se corrigio la manera en como se calculaba el tiempo dentro de los cuadrantes, se agrego para visualizar el centroide del objeto, se agrego blur para eliminar posibles esquinas, se agrego el conteo de entrada y salida de diferentes cuadrantes, se reestructuro el main loop para que no fuera tan extenso y volverlo más sencillo de leer. Falta corregir la dispersion del centoride ya que varia mucho de posicion, agregar el frame del objeto detectado, agregar espacios de colores para hacerlo mas robusto y creo que ya.

--- hm10.py
+++ hm10.py
@@ -31,11 +31,13 @@ def resize_image(img, resize_percent):
     return resized_img
 
 def extract_corners_and_descriptors(image):
+    # Aplicar un filtro Gaussiano para suavizar la imagen
+    image = cv.GaussianBlur(image, (7, 7), 0)
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    harris_corners = cv.cornerHarris(gray, 2, 9, 0.04)
-    threshold = 0.01 * harris_corners.max()
+    harris_corners = cv.cornerHarris(gray, 2, 3, 0.05)
+    threshold = 0.10 * harris_corners.max()  # Aumentar el umbral
     corner_positions = np.argwhere(harris_corners > threshold)
-    keypoints = [cv.KeyPoint(float(x[1]), float(x[0]), 3) for x in corner_positions]
+    keypoints = [cv.KeyPoint(float(x[1]), float(x[0]), 5) for x in corner_positions]
     brief = cv.xfeatures2d.BriefDescriptorExtractor_create()
     keypoints, descriptors = brief.compute(gray, keypoints)
     return keypoints, descriptors, corner_positions
@@ -46,29 +48,27 @@ def calculate_centroid(keypoints):
     x = np.mean([kp.pt[0] for kp in keypoints])
     y = np.mean([kp.pt[1] for kp in keypoints])
     return (x, y)
-
 def determine_quadrant(centroid, width, height):
     mid_x = width / 2
-    # Ecuación de la línea diagonal: y = mx + b
-    m = height / width
-    b = 0  # la línea pasa por el origen
+    m = -height / width  # pendiente negativa desde la esquina superior derecha a la inferior izquierda
+    b = height  # intercepta en el borde superior derecho (y = mx + b para x = 0)
     x, y = centroid
     diagonal_line_y = m * x + b
 
-    if x < mid_x and y > diagonal_line_y:
-        return 3  # Cuadrante inferior izquierdo
+    if x > mid_x and y < diagonal_line_y:
+        return 1  # Cuadrante superior derecho
     elif x < mid_x and y < diagonal_line_y:
-        return 1  # Cuadrante superior izquierdo
-    elif x > mid_x and y > diagonal_line_y:
-        return 4  # Cuadrante inferior derecho
+        return 2  # Cuadrante superior izquierdo
+    elif x < mid_x and y > diagonal_line_y:
+        return 3  # Cuadrante inferior izquierdo
     else:
-        return 2  # Cuadrante superior derecho
+        return 4  # Cuadrante inferior derecho
 
 
 def draw_matches(img1, kp1, des1, img2, kp2, des2):
     bf = cv.BFMatcher()
     matches = bf.knnMatch(des1, des2, k=2)
-    good_matches = [m for m, n in matches if m.distance < 0.75 * n.distance]
+    good_matches = [m for m, n in matches if m.distance < 0.80 * n.distance]
 
     if len(good_matches) >= 4:  # Asegúrate de tener al menos 4 buenos matches
         # Extraer las coordenadas de los buenos matches
@@ -76,7 +76,7 @@ def draw_matches(img1, kp1, des1, img2, kp2, des2):
         dst_pts = np.float32([kp2[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
 
         # Estimar la homografía con RANSAC
-        M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC, 5.0)
+        M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC, 7.0)
         matchesMask = mask.ravel().tolist()
 
         # Dibujar solo los matches que son inliers
@@ -92,48 +92,56 @@ def draw_matches(img1, kp1, des1, img2, kp2, des2):
         
     return matched_img
 
-def print_time_in_seconds(quadrant_times, fps):
-    time_in_seconds = {k: v / fps for k, v in quadrant_times.items()}
-    print("Time spent in quadrants (seconds):", time_in_seconds)
+def print_time_and_counts(quadrant_times, fps):
+    time_in_seconds = {k: {'time': v['time'] / fps, 'count': v['count']} for k, v in quadrant_times.items()}
+    print("Time spent and entry counts in quadrants:", time_in_seconds)
+
+def initialize_capture(video_path):
+    cap = cv.VideoCapture(video_path if video_path != "0" else 0)
+    fps = cap.get(cv.CAP_PROP_FPS)
+    return cap, fps
+
+def process_frame(frame, resize_percent, reference_img_resized, ref_kp, ref_des, last_quadrant, quadrant_times):
+    frame_resized = resize_image(frame, resize_percent)
+    kp, des, corner_positions = extract_corners_and_descriptors(frame_resized)
+    if kp:
+        centroid = calculate_centroid(kp)
+        if centroid:
+            x, y = int(centroid[0]), int(centroid[1])
+            cv.circle(frame_resized, (x, y), 5, (255, 0, 0), -1)
+            quadrant = determine_quadrant(centroid, frame_resized.shape[1], frame_resized.shape[0])
+            if quadrant != last_quadrant:
+                quadrant_times[quadrant]['count'] += 1
+                last_quadrant = quadrant
+            quadrant_times[quadrant]['time'] += 1
+    draw_lines(frame_resized)
+    matched_img = draw_matches(frame_resized, kp, des, reference_img_resized, ref_kp, ref_des)
+    return matched_img, last_quadrant
+
+def draw_lines(frame):
+    height, width = frame.shape[:2]
+    cv.line(frame, (0, height), (width, 0), (0, 255, 0), 2)
+    cv.line(frame, (width // 2, 0), (width // 2, height), (255, 255, 0), 2)
 
 def main_loop(video_path, reference_img, resize_percent):
-    cap = cv.VideoCapture(video_path if video_path != "0" else 0)
-    fps = cap.get(cv.CAP_PROP_FPS)  # Obtiene el FPS del video
+    cap, fps = initialize_capture(video_path)
     reference_img_resized = resize_image(reference_img, resize_percent)
     ref_kp, ref_des, ref_corner_positions = extract_corners_and_descriptors(reference_img_resized)
-
-    # Dibujar todas las esquinas detectadas en la imagen de referencia
-    for pos in ref_corner_positions:
-        cv.circle(reference_img_resized, (pos[1], pos[0]), 5, (0, 255, 255), 1)  # Dibuja un círculo amarillo en cada esquina
-
-    quadrant_times = {1: 0, 2: 0, 3: 0, 4: 0}
+    quadrant_times = {1: {'time': 0, 'count': 0}, 2: {'time': 0, 'count': 0}, 3: {'time': 0, 'count': 0}, 4: {'time': 0, 'count': 0}}
+    last_quadrant = None
 
     while True:
         ret, frame = cap.read()
         if not ret:
             break
-
-        frame_resized = resize_image(frame, resize_percent)
-        kp, des, corner_positions = extract_corners_and_descriptors(frame_resized)
-
-        # Dibujar todas las esquinas detectadas en el frame
-        for pos in corner_positions:
-            cv.circle(frame_resized, (pos[1], pos[0]), 5, (0, 255, 255), 1)  # Dibuja un círculo amarillo en cada esquina
-
-        matched_img = draw_matches(frame_resized, kp, des, reference_img_resized, ref_kp, ref_des)
-
-        centroid = calculate_centroid(kp)
-        if centroid:
-            quadrant = determine_quadrant(centroid, frame_resized.shape[1], frame_resized.shape[0])
-            quadrant_times[quadrant] += 1
-
+        matched_img, last_quadrant = process_frame(frame, resize_percent, reference_img_resized, ref_kp, ref_des, last_quadrant, quadrant_times)
         cv.imshow('Real-time Feature Matching', matched_img)
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
 
     cap.release()
     cv.destroyAllWindows()
-    print_time_in_seconds(quadrant_times, fps)
+    print_time_and_counts(quadrant_times, fps)
 
 def run_pipeline():
     args = user_interaction()
